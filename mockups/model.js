5b23a42724479988220f1b127bb735b094ac8308
bert-janzwanepol <bert-janzwanepol@hotmail.com>
2019-10-23 10:27:01 +0200

    add mongoose model

---
+++ mockups/model.js
@@ -0,0 +1,38 @@
+let gameSchema = new mongoose.Schema({
+    gameName: String,
+    teams: [teamSchema],
+    rounds: [roundQuestionSchema],
+    quizMaster: String,
+    password: String
+});
+
+let teamSchema = new mongoose.Schema({
+    teamName: String,
+    score: Number
+})
+
+let roundQuestionSchema = new mongoose.Schema({
+    question: [{
+        _id: String, // Link naar questionSchema
+        answers: [answerSchema],
+        closed: Boolean
+    }]
+})
+
+let answerSchema = new mongoose.Schema({
+    answer: [{
+        teamName: String,
+        answer: String,
+        correct: true
+    }]
+})
+
+let questionSchema = new mongoose.Schema({
+    question: String,
+    answer: String,
+    category: String
+})
+
+let quizMasterSchema = new mongoose.Schema({
+    name: String
+})
