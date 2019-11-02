91813645ab9b9f0d731c50c63ce75bbf25bf6861
Bert-Jan <bert-janzwanepol@hotmail.com>
2019-11-02 15:10:54 +0100

    setup redux

--- team-app/src/index.js
+++ team-app/src/index.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
+
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
+import { mainReducer } from './reducers/application';
+
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const logger = (store) => (next) => (action) => {
+//     console.log('ACTION:', action.type, action);
+//     let result = next(action);
+//     console.log('STATE AFTER ACTION:', action.type, store.getState());
+//     return result;
+// }
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || Redux.compose;
+const theStore = Redux.createStore(mainReducer, composeEnhancers(
+    Redux.applyMiddleware(thunk)
+));
+
+const mainComponent =
+    <ReactRedux.Provider store={theStore}>
+        <App />
+    </ReactRedux.Provider>
+
+ReactDOM.render(mainComponent, document.getElementById('root'));
