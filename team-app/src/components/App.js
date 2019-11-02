1ecca5abc86de0d4b2cb3cbf86a854995330ebe3
Bert-Jan <bert-janzwanepol@hotmail.com>
2019-11-02 15:11:29 +0100

    iniital signup flow - roomkey + teamname

--- team-app/src/components/App.js
+++ team-app/src/components/App.js
@@ -1,49 +1,35 @@
-import React, { useState } from 'react';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
 import '../App.css';
 import Logo from './Logo';
 import SignupForm from './Signup';
 import TitleMessage from './TitleMessage';
-import LoadingIndicator from './LoadingIndicator';
-import Question from './Question';
-import Result from './Result';
+// import LoadingIndicator from './LoadingIndicator';
+// import Question from './Question';
+// import Result from './Result';
+
 
 function App() {
-  const [status, setStatus] = useState(true);
-
   return (
-    <>
+    <Router>
       <div className="app">
-        <header>
-          <Logo />
-          <TitleMessage title="Please enter a teamname :)" />
-        </header>
-        <SignupForm />
+        <Switch>
+          <Route exact path="/">
+            <header>
+              <Logo />
+              <TitleMessage title="Please enter a teamname :)" />
+            </header>
+            <SignupForm />
+          </Route>
+        </Switch>
       </div>
-      <div className="app">
-        <header>
-          <Logo />
-          <TitleMessage title="Waiting for the Quizz Master to approve your teamname." />
-        </header>
-        <LoadingIndicator />
-      </div>
-      <div className="app">
-        <header>
-          <Logo />
-          <TitleMessage title="Waiting for next question." />
-        </header>
-        <LoadingIndicator />
-      </div>
-      <div className="app">
-        <header>
-          <div className="logo-symbol">Q</div>
-        </header>
-        <Question question="Who wrote the poem 'The Owl and the Pussycat'?" />
-      </div>
-      <div className="app">
-        <Result answer={status} />
-      </div>
-      <button onClick={() => setStatus(!status)}>change answer status</button>
-    </>
+    </Router>
   );
 }
 
--- team-app/src/components/Signup.js
+++ team-app/src/components/Signup.js
@@ -1,16 +1,42 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import * as ReactRedux from 'react-redux';
 
-const SignupForm = () => {
+import { setTeamNameAction, setRoomPasswordAction, submitTeam } from '../reducers/application'
 
-    const [teamName, setTeamName] = useState("");
+class SignupFormUI extends Component {
+    render() {
+        return (
+            <form>
+                <label htmlFor="teamName">Teamnaam</label>
+                <input value={this.props.teamName} onChange={e => this.props.setTeamName(e.target.value)} type="text" name="teamName" id="teamName" required />
+                {this.props.errorMessage && <small className='form-error-message'>{this.props.errorMessage}</small>}
+                <label htmlFor="roomKey">Roomkey</label>
+                <input value={this.props.roomkey} onChange={e => this.props.setRoomPassword(e.target.value)} type="text" name="roomKey" id="roomKey" required />
+                <button type="submit" onClick={(e) => this.props.submitTeam(this.props.teamName, this.props.roomkey, e)}>create team</button>
+            </form>
+        );
+    }
 
-    return (
-        <form>
-            <input value={teamName} onChange={e => setTeamName(e.target.value)} type="text" name="teamName" required />
-            <small className='form-error-message'>Error text komt hier</small>
-            <button type="submit">create team</button>
-        </form>
-    );
 }
+
+
+const mapStateToProps = (state) => {
+    return {
+        awaitingConfirmation: state.application.awaitingConfirmation,
+        roomkey: state.application.roomkey,
+        teamName: state.application.teamName,
+        errorMessage: state.application.errorMessage
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setTeamName: (name) => dispatch(setTeamNameAction(name)),
+        setRoomPassword: (password) => dispatch(setRoomPasswordAction(password)),
+        submitTeam: (name, roomkey, event) => dispatch(submitTeam(name, roomkey, event))
+    }
+}
+
+const SignupForm = ReactRedux.connect(mapStateToProps, mapDispatchToProps)(SignupFormUI);
 
 export default SignupForm;
