c924e823d141ad513464bf4b9e4895af768231b1
Bert-Jan <bert-janzwanepol@hotmail.com>
2019-11-04 19:09:45 +0100

    updated action so it sends the roomkey back

--- server/routes/teams.js
+++ server/routes/teams.js
@@ -18,7 +18,7 @@ router.post('/', (req, res) => {
 
         game.addTeam(req.body.name)
         res.json({ token: token })
-        req.app.get('wss').broadcast({ type: 'TEAMCHANGE' }, game.password, 'quizmaster')
+        req.app.get('wss').broadcast({ type: 'TEAMCHANGE', roomkey: game.password }, game.password, 'quizmaster')
     }
 })
 
@@ -36,7 +36,7 @@ router.put('/:teamname/approve', async (req, res) => {
         game.save()
         res.status(200).send()
 
-        req.app.get('wss').broadcast({ type: 'TEAMCHANGE' }, game.password, 'quizmaster')
+        req.app.get('wss').broadcast({ type: 'TEAMCHANGE', roomkey: game.password }, game.password, 'quizmaster')
         req.app.get('wss').sendToTeam({ type: 'TEAMCHANGE' }, game.password, team.name)
     } else {
         const err = { code: "RESNOTFOUND" }
