76bbc014afe9a8223e7483c87fa7cc9e7cc36e8f
Bert-Jan <bert-janzwanepol@hotmail.com>
2019-11-04 19:13:03 +0100

    implemented socket middleware in quizmaster app

--- quizmaster-app/src/index.js
+++ quizmaster-app/src/index.js
@@ -8,10 +8,29 @@ import thunk from 'redux-thunk';
 import * as Redux from 'redux';
 import * as ReactRedux from 'react-redux';
 import mainReducer from './reducers';
+import socketMiddleware from './middleware/socket';
+
+import reduxWebsocket from '@giantmachines/redux-websocket';
+
+// send the first message on open to authenticate the websocket
+const websocketOptions = {
+    onOpen: (socket) => {
+
+        let initMessage = {
+            initial: true,
+            password: theStore.getState().game.game.password,
+            token: sessionStorage.getItem('token')
+        }
+
+        socket.send(JSON.stringify(initMessage))
+    }
+}
+const reduxWebsocketMiddleware = reduxWebsocket(websocketOptions);
+
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || Redux.compose;
 const theStore = Redux.createStore(mainReducer, composeEnhancers(
-    Redux.applyMiddleware(thunk)
+    Redux.applyMiddleware(thunk, reduxWebsocketMiddleware, socketMiddleware)
 ));
 
 const mainComponent =
