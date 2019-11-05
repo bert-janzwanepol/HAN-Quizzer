8813ec1221a34df938635f6b8dd3fe2a410dabe8
Hendrik <hendrik.camps@outlook.com>
2019-11-05 11:00:54 +0100

    dunno

--- server/routes/questions.js
+++ server/routes/questions.js
@@ -6,7 +6,6 @@ const Question = mongoose.model('Question')
 
 router.get('/:questionId', async (req, res) => {
     const { question, category } = await Question.findOne({ _id: req.params.questionId })
-    console.log(question, category)
     res.json({ question, category })
 })
 
